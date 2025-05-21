fc6300e84e2f763a75f38b27dddd337af2ba8b5a
harshad1 <dahakeharshad10@gmail.com>
2025-05-21 16:14:47 -0700

    first commit ever - harshad1

---
+++ firstLecture.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 21 15:34:29 2025
+
+@author: harshadahake
+"""
+
+import pandas as pd
+
+data = pd.read_csv('winequality-red.csv')
+
+print(data.head())
+
