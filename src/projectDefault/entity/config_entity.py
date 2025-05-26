1496d039e4146b9d4558164003bfc5b28eb7e323
rooman10 <dahakeharshad10@gmail.com>
2025-05-25 20:38:33 -0700

    data ingestion pipeline

---
+++ src/projectDefault/entity/config_entity.py
@@ -0,0 +1,1 @@
+# define an entity (returned by function)from dataclasses import dataclassfrom pathlib import Path@dataclass(frozen=True)class DataIngestionConfig:    root_dir: Path    source_URL: str    local_data_file: Path    unzip_dir: Path
